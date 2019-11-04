068bb1149d412ba7092554e023c7d66f898d1360
legacy-gopher-9954 <fabianjohnson07@gmail.com>
2019-11-04 19:26:36 +0000

    Automatically backed up by Learn

---
+++ lib/intro_to_simple_array_manipulations.rb
@@ -0,0 +1,51 @@
+
+
+def using_push(array)
+  entertainers = ["Lafayette", "Sookie", "Bill"]
+  entertainers.push
+end
+
+def using_unshift
+  entertainers = ["Lafayette", "Sookie", "Bill"]
+  entertainers.unshift
+end
+
+def using_pop
+  entertainers = ["Lafayette", "Sookie", "Bill"]
+  entertainers.pop
+end
+
+def shift_with_args
+  entertainers = ["Lafayette", "Sookie", "Bill"]
+  entertainers.shift
+end  
+
+def using con_cat
+  entertainers = ["Lafayette", "Sookie", "Bill"]
+  entertainers.concat
+end
+
+def using_insert
+  entertainers = ["Lafayette", "Sookie", "Bill"]
+  entertainers.insert
+end
+
+def using_uniq
+  entertainers = ["Lafayette", "Sookie", "Bill"]
+  entertainers.uniq
+end
+
+def using flatten
+  entertainers = ["Lafayette", "Sookie", "Bill"]
+  entertainers.flatten
+end
+
+def using_delete
+  entertainers = ["Lafayette", "Sookie", "Bill"]
+  entertainers.delete
+end
+
+def using_delete_at
+  entertainers = ["Lafayette", "Sookie", "Bill"]
+  entertainers.integer[1]
+end  
